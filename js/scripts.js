eebb1fbe43c0b4a29586b51f16234600fc7d9f6c
Derek Villars and Shane Villars <derek@>
2017-01-09 11:13:57 -0800

    added bmi calculation

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,11 @@ function div(num1, num2) {
 	return x;
 };
 
+function bmi(height, weight) {
+  var x = weight / (height * height);
+  return x;
+};
+
 function ask(){
 	var num1 = parseInt(prompt('Enter a number:'));
   var num2 = parseInt(prompt('Enter another number:'));
@@ -28,3 +33,12 @@ function ask(){
 };
 
 ask();
+
+
+function askbmi() {
+  var height = parseFloat(prompt('Enter your height in meters:'));
+  var weight = parseFloat(prompt('Enter your weight in kilograms:'));
+  alert(bmi(height, weight));
+};
+
+askbmi();
